8eb403a184c91809f948a6951b1c5415b79e6efb
Danesh M <daneshm90@gmail.com>
2012-08-06 22:09:20 -0400

    Initial commit

---
+++ recreate.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+if [ $# -le 2 -o $# -gt 3 ]; then
+    echo "Usage : <dup file> <where to place files> <blob folder>"
+    exit
+fi
+if [ ! -f "$1" ]; then
+    echo "$1" does not exist
+fi
+if [ -d "$2" ]; then
+    read -p "\"$2\" already exists, want to overwrite (y/n) ? : " ans
+    if [ "$ans" != "y" ]; then
+        exit
+    fi
+else
+    mkdir $2
+fi
+if [ ! -d "$3" ]; then
+    echo "$3" does not exist
+    exit
+fi
+while read line
+do
+if [ "$line" != "dedupe	2" ]; then
+    type=$(echo $line | cut -d " " -f1)
+    item=$(echo $line | cut -d " " -f8)
+    hashFolder=$(echo $line | cut -d " " -f9 | cut -d "/" -f1)
+    hashFile=$(echo $line | cut -d " " -f9 | cut -d "/" -f2)
+    if [ "$type" = "f" -a ! -z "$hashFolder" -a ! -z "$hashFile" -a -f "$3/$hashFolder/$hashFile" ]; then
+        if [ -f "$3/$hashFolder/$hashFile" ]; then
+            cp "$3/$hashFolder/$hashFile" "test/$item"
+        else
+            echo "No blob for " $item " found"
+        fi
+    else
+        mkdir -p test/$item
+    fi
+fi
+done < "$1"
