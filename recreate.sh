8bc3b5677b32561a4bac29f8fcd0d7e8a86e5bdd
Danesh M <daneshm90@gmail.com>
2012-08-06 22:29:09 -0400

    Improve clarity

--- recreate.sh
+++ recreate.sh
@@ -1,38 +1,54 @@
 #!/bin/sh
+
+#Validate number of arguments
 if [ $# -le 2 -o $# -gt 3 ]; then
     echo "Usage : <dup file> <where to place files> <blob folder>"
     exit
 fi
-if [ ! -f "$1" ]; then
-    echo "$1" does not exist
+
+#Assign arguments to variables for clarity
+dupFile="$1"
+outputFolder="$2"
+blobFolder="$3"
+
+#Confirm dupFile is present
+if [ ! -f "$dupFile" ]; then
+    echo "$dupFile" does not exist
 fi
-if [ -d "$2" ]; then
-    read -p "\"$2\" already exists, want to overwrite (y/n) ? : " ans
+
+#Check if outputFolder already exists, if it does then ask for overwrite
+#permission. If it doesnt exist, create it.
+if [ -d "$outputFolder" ]; then
+    read -p "\"$outputFolder\" already exists, want to overwrite (y/n) ? : " ans
     if [ "$ans" != "y" ]; then
         exit
     fi
 else
-    mkdir $2
+    mkdir $outputFolder
 fi
-if [ ! -d "$3" ]; then
-    echo "$3" does not exist
+
+#Confirm blobFolder is present
+if [ ! -d "$blobFolder" ]; then
+    echo "$blobFolder" does not exist
     exit
 fi
-while read line
-do
+
+#Loop through dupFile and parse its contents.
+#Copy and rename the blobs and create the tree accordingly.
+while read line; do
 if [ "$line" != "dedupe	2" ]; then
     type=$(echo $line | cut -d " " -f1)
     item=$(echo $line | cut -d " " -f8)
     hashFolder=$(echo $line | cut -d " " -f9 | cut -d "/" -f1)
     hashFile=$(echo $line | cut -d " " -f9 | cut -d "/" -f2)
     if [ "$type" = "f" -a ! -z "$hashFolder" -a ! -z "$hashFile" ]; then
-        if [ -f "$3/$hashFolder/$hashFile" ]; then
-            cp "$3/$hashFolder/$hashFile" "$2/$item"
+        if [ -f "$blobFolder/$hashFolder/$hashFile" ]; then
+            cp "$blobFolder/$hashFolder/$hashFile" "$outputFolder/$item"
         else
             echo "No blob for " $item " found"
         fi
     else
-        mkdir -p $2/$item
+        mkdir -p $outputFolder/$item
     fi
 fi
-done < "$1"
+done < "$dupFile"
